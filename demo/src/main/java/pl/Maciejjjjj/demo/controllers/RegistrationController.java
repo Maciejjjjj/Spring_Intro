990d307fc7b4a7686be2c18b326bf5ebab34ee52
maciej <sucheckimaciej3@gmail.com>
2019-10-19 14:41:44 +0200

    2.6.

--- demo/src/main/java/pl/Maciejjjjj/demo/controllers/RegistrationController.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/controllers/RegistrationController.java
@@ -1,6 +1,7 @@
 package pl.Maciejjjjj.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +15,16 @@ public class RegistrationController {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+
 
     @GetMapping
     public String prepareRegistrationPage() {
@@ -26,9 +33,12 @@ public class RegistrationController {
 
     @PostMapping
     public String processRegistrationPage(String username, String password, String firstName, String lastName) {
+
+        String encdoedPassword = passwordEncoder.encode(password);
+
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(encdoedPassword);
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setActive(true);
