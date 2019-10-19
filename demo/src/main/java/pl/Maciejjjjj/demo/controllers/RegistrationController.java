88c24cb6a6d147056e91be44e084f78551d3a4dc
maciej <sucheckimaciej3@gmail.com>
2019-10-19 14:13:54 +0200

    2.4.

--- demo/src/main/java/pl/Maciejjjjj/demo/controllers/RegistrationController.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/controllers/RegistrationController.java
@@ -1,13 +1,23 @@
 package pl.Maciejjjjj.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.Maciejjjjj.demo.model.domain.User;
+import pl.Maciejjjjj.demo.model.repositories.UserRepository;
 
 @Controller
 @RequestMapping("/register")
 public class RegistrationController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public RegistrationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
     public String prepareRegistrationPage() {
@@ -15,8 +25,17 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String processRegistrationPage() {
-        return "";
+    public String processRegistrationPage(String username, String password, String firstName, String lastName) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setActive(true);
+
+        userRepository.save(user);
+
+        return "redirect:/index.html";
     }
 }
 
