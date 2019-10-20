d4d3228095d0d93c1cac16b24f0ff9a589f761e0
maciej <sucheckimaciej3@gmail.com>
2019-10-20 15:43:45 +0200

    3.3.

---
+++ demo/src/main/java/pl/Maciejjjjj/demo/controllers/HomePageController.java
@@ -0,0 +1,9 @@
+package pl.Maciejjjjj.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+}
--- demo/src/main/java/pl/Maciejjjjj/demo/model/repositories/AdvertRepository.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/model/repositories/AdvertRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.Maciejjjjj.demo.model.domain.Advert;
 
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
-    
+
 }
