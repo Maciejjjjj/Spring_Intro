46123b6b07da1fe69394f8ed1ff0baf8fb2ed249
maciej <sucheckimaciej3@gmail.com>
2019-10-20 13:55:44 +0200

    A.3.4.

--- spring-core/src/main/java/pl/Maciejjjjj/spring/core/app/PrinterApplication.java
+++ spring-core/src/main/java/pl/Maciejjjjj/spring/core/app/PrinterApplication.java
@@ -2,22 +2,27 @@ package pl.Maciejjjjj.spring.core.app;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.Maciejjjjj.spring.core.components.printers.Printer;
+import pl.Maciejjjjj.spring.core.warmup.HelloWorld;
 
 public class PrinterApplication {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("pl.Maciejjjjj.spring.core.components");
+        context.scan("pl.Maciejjjjj.spring.core.components",
+                "pl.Maciejjjjj.spring.core.warmup");
         context.refresh();
 
-        Printer consolePrinter = context.getBean("consolePrinter", Printer.class);
-        consolePrinter.print("Elo 3 2 0");
+//        Printer consolePrinter = context.getBean("consolePrinter", Printer.class);
+//        consolePrinter.print("Elo 3 2 0");
+//
+//        Printer filePrinter = context.getBean("filePrinter", Printer.class);
+//        filePrinter.print("Elo 3 2 0");
+//
+//        Printer dialogPrinter = context.getBean("dialogPrinter", Printer.class);
+//        dialogPrinter.print("Elo 3 2 0");
 
-        Printer filePrinter = context.getBean("filePrinter", Printer.class);
-        filePrinter.print("Elo 3 2 0");
-
-        Printer dialogPrinter = context.getBean("dialogPrinter", Printer.class);
-        dialogPrinter.print("Elo 3 2 0");
+        HelloWorld helloWorld = context.getBean("helloWorld", HelloWorld.class);
+        helloWorld.sayHello();
 
     }
 
--- spring-core/src/main/java/pl/Maciejjjjj/spring/core/warmup/HelloWorld.java
+++ spring-core/src/main/java/pl/Maciejjjjj/spring/core/warmup/HelloWorld.java
@@ -1,13 +1,46 @@
 package pl.Maciejjjjj.spring.core.warmup;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.Maciejjjjj.spring.core.components.printers.ConsolePrinter;
+import pl.Maciejjjjj.spring.core.components.printers.DialogPrinter;
+import pl.Maciejjjjj.spring.core.components.printers.Printer;
+
+import java.util.Random;
 
 @Component
 public class HelloWorld {
 
-public void sayHello() {
+    private ConsolePrinter consolePrinter;
+    private DialogPrinter dialogPrinter;
+    private Printer somePrinter;
 
-        System.out.println("Hello world!");
+    @Autowired
+    public HelloWorld(ConsolePrinter consolePrinter) {
+        this.consolePrinter = consolePrinter;
+    }
+
+    @Autowired
+    public void setDialogPrinter(DialogPrinter dialogPrinter) {
+        this.dialogPrinter = dialogPrinter;
+    }
+
+    @Autowired
+    public void setSomePrinter(Printer somePrinter) {
+        this.somePrinter = somePrinter;
+    }
+
+    public HelloWorld() {
+
+    }
+
+    public void sayHello() {
+
+        if (new Random().nextInt(2) > 0) {
+            consolePrinter.print("Hello, world!");
+        } else {
+            dialogPrinter.print("Hello, world!");
+        }
 
     }
 
