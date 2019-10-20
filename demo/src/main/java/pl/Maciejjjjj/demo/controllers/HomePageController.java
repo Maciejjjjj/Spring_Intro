10ce681dc3bb1b149ae7e26de8be43fe4622310c
maciej <sucheckimaciej3@gmail.com>
2019-10-20 15:56:56 +0200

    3.4.

--- demo/src/main/java/pl/Maciejjjjj/demo/controllers/HomePageController.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/controllers/HomePageController.java
@@ -1,9 +1,28 @@
 package pl.Maciejjjjj.demo.controllers;
 
+import com.sun.tools.javac.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.Maciejjjjj.demo.model.domain.Advert;
+import pl.Maciejjjjj.demo.model.repositories.AdvertRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomePageController {
+
+    private final AdvertRepository advertRepository;
+
+    public HomePageController(AdvertRepository advertRepository) {
+        this.advertRepository = advertRepository;
+    }
+
+    @GetMapping
+    public String prepareHomePage(Model model) {
+        List<Advert> allAdverts = advertRepository.findAllByOrderByPostedDesc();
+        model.addAttribute("adverts", allAdverts);
+        return "";
+    }
 }
--- demo/src/main/java/pl/Maciejjjjj/demo/model/repositories/AdvertRepository.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/model/repositories/AdvertRepository.java
@@ -1,8 +1,11 @@
 package pl.Maciejjjjj.demo.model.repositories;
 
+import com.sun.tools.javac.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.Maciejjjjj.demo.model.domain.Advert;
 
 public interface AdvertRepository extends JpaRepository<Advert, Long> {
+
+    List<Advert> findAllByOrderByPostedDesc();
 
 }
