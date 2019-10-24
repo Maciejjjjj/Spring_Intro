37960aa83b301e9b23f76379f5ee42aabcca3b01
maciej <sucheckimaciej3@gmail.com>
2019-10-24 18:02:37 +0200

    3.9.

--- demo/src/main/java/pl/Maciejjjjj/demo/controllers/LoginControler.java
+++ demo/src/main/java/pl/Maciejjjjj/demo/controllers/LoginControler.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.Maciejjjjj.demo.model.domain.User;
 import pl.Maciejjjjj.demo.model.repositories.UserRepository;
 
 @Controller
